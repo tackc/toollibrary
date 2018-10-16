869f110416803803012d734dd24f1912e7ede9b9
Chris Tack <chris_tack@mac.com>
2018-10-16 12:53:11 -0700

    working on login form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,9 +1,14 @@
 from django.forms import ModelForm, Form, CharField, IntegerField, PasswordInput
-from .models import Tool, Profile, Category
+from .models import Tool, Profile, Category, User
 
 class LoginForm(Form):
     username = CharField(label="User Name", max_length=64)
     password = CharField(widget=PasswordInput())
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
 
 class ProfileForm(ModelForm):
     class Meta:
--- main_app/views.py
+++ main_app/views.py
@@ -29,10 +29,10 @@ class ToolDelete(DeleteView):
     model = Tool
     success_url = '/tools'
 
-# def update_profile(request, user_id):
-#     user = User.objects.get(pk=user_id)
-#     user.profile.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...'
-#     user.save()
+def update_profile(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user.profile.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...'
+    user.save()
 
 def index(request):
     return render(request, 'index.html')
